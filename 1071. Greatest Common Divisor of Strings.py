1528b77175b90aee6903d15c69c7b0c6cf3a5684
leonado10000 <rjdispair@gmail.com>
2023-02-01 23:15:53 +0530

    Create 1071. Greatest Common Divisor of Strings.py
    For two strings s and t, we say "t divides s" if and only if s = t + ... + t (i.e., t is concatenated with itself one or more times).
    
    Given two strings str1 and str2, return the largest string x such that x divides both str1 and str2.

---
+++ 1071. Greatest Common Divisor of Strings.py
@@ -0,0 +1,20 @@
+#1071. Greatest Common Divisor of Strings
+def gcdOfStrings(self, str1: str, str2: str) -> str:
+    fin = ""
+    m = min(len(str1),len(str2))
+    gin = []
+    for i in range(1,m+1):
+        if len(str1)%i == 0 and len(str2) % i == 0:
+            gin.append(i)
+    
+    for i in gin:
+        #check for str1
+        a = str2[0:i]
+        if str1 == a*int(len(str1)/i) and str2 == a*int(len(str2)/i):
+            fin = a
+
+    return fin
+
+
+
+print(gcdOfStrings(5,str1 = "ABCABC", str2 = "ABC"))
